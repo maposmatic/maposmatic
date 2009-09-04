cd043ef4666e1cd10d915c0b03802654c5d521ad
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2009-09-04 08:55:32 +0200

    Add missing context_processors

---
+++ www/maposmatic/context_processors.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+# maposmatic, the web front-end of the MapOSMatic city map generation system
+# Copyright (C) 2009  David Decotigny
+# Copyright (C) 2009  Frédéric Lehobey
+# Copyright (C) 2009  David Mentré
+# Copyright (C) 2009  Maxime Petazzoni
+# Copyright (C) 2009  Thomas Petazzoni
+# Copyright (C) 2009  Gaël Utard
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from models import MapRenderingJob
+
+def all(request):
+    d = {}
+    d['randommap'] = MapRenderingJob.objects.get_random_with_thumbnail()
+    return d
