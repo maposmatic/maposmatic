2e3620b3c45ab5be41c0c57cf8eb0758ad138191
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-01-08 00:01:15 +0100

    Add Nederlands to the list of supported languages for the website
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- www/settings.py
+++ www/settings.py
@@ -112,7 +112,8 @@ LANGUAGES = (("fr", u"Français"),
              ("ca", u"Català"),
              ("ru", u"Русский"),
              ("ar", u"عربية"),
-             ("pt_BR", u"Português do Brasil"))
+             ("pt_BR", u"Português do Brasil"),
+             ("nl", u"Nederlands"))
 
 MAP_LANGUAGES = [("fr_BE.UTF-8", u"Royaume de Belgique (FR)"),
                  ("fr_FR.UTF-8", u"France"),
