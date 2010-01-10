3618d1a7969a195c073061e5fb91b4e068084882
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2010-01-10 15:54:08 +0100

    New automatic suggestion mechanism
    The administrative city input field now features an automatic
    suggestion drop down list from which the user can choose the city he
    wants.
    
    The Javascript has been greatly rewritten to make better usage of JQuery
    and for cleaner, more concise code.

--- www/media/osm_map.js
+++ www/media/osm_map.js
@@ -72,7 +72,7 @@ function updateMap()
 }
 
 /* Main initialisation function. Must be called before the map is manipulated. */
-function init()
+function mapInit()
 {
     map = new OpenLayers.Map ('map', {
         controls:[new OpenLayers.Control.Navigation(),
@@ -91,5 +91,178 @@ function init()
     map.events.register('zoomend', map, updateForm);
     map.events.register('moveend', map, updateForm);
     updateMap();
+}
+
+function switchToAdminMode() {
+  $('#mapform tbody').children('tr.bybbox').hide();
+  $('#mapform tbody').children('tr.byadmin').show();
+  $('#map_language_label').hide();
+  $('#map_language_entry').hide();
+}
+
+function switchToBBoxMode() {
+  $('#mapform tbody').children('tr.byadmin').hide();
+  $('#mapform tbody').children('tr.bybbox').show();
+  $('#map_language_label').show();
+  $('#map_language_entry').show();
+  if (map == null)
+    mapInit();
+}
+
+$(document).ready(function() {
+  $('#id_mode_0').bind('click', function(e) { switchToAdminMode(); });
+  $('#id_mode_1').bind('click', function(e) { switchToBBoxMode(); });
+
+  if ($('#id_mode_1').is(':checked'))
+    switchToBBoxMode();
+  else
+    switchToAdminMode();
+
+  suggest('#id_administrative_city', '#suggest',
+          '#id_administrative_osmid', '#id_go_next_btn',
+          { selectedClass: 'selected',
+            timeout: 150
+          });
+});
+
+function suggest(input, results, osm_id, button, options) {
+  var $input = $(input).attr('autocomplete', 'off');
+  var $results = $(results);
+  var $osm_id = $(osm_id);
+  var $button = $(button);
+  var timeout = false;
+
+  // Setup the keyup event.
+  $input.keyup(processKey);
+
+  // Disable form validation via the Enter key
+  $input.keypress(function(e) { if (e.keyCode == 13) return false; });
+
+  function appendValidResult(id, name) {
+    $results.append('<li class="suggestok" id="rad' + id + '">'
+       + name + '</span></li>');
+    return $('#rad' + id);
+  }
+
+  function handleNominatimResults(data, textResult) {
+    closeSuggest();
+
+    if (data.length)
+      $results.show();
+
+    $.each(data, function(i, item) {
+      if (typeof item.ocitysmap_params != 'undefined' &&
+          item.ocitysmap_params['admin_level'] == 8) {
+        var e = appendValidResult(item.ocitysmap_params['id'], item.display_name);
+
+        e.bind('click', function(e) { setResult($(this)); });
+        e.bind('onmouseover', function(e) { setSelectedResultTo($(this)); });
+      } else {
+        $results.append('<li class="suggestoff">'
+          + item.display_name + '</li>');
+      }
+    });
+  }
+
+  function processKey(e) {
+    clearResult();
+
+    switch (e.keyCode) {
+      case 27:  // ESC
+        closeSuggest();
+        break;
+      case 9:   // TAB
+      case 13:  // OK
+        selectCurrentResult();
+        return false;
+        break;
+      case 38:  // UP
+        prevResult();
+        break;
+      case 40:  // DOWN
+        nextResult();
+        break;
+      default:
+        if (timeout) {
+          clearTimeout(timeout);
+        }
+        timeout = setTimeout(function() {
+          $.getJSON("/nominatim/", {q: $input.val()},
+            handleNominatimResults);
+          }, options.timeout);
+      }
+  }
+
+  function clearResult() {
+    $osm_id.val('');
+    $button.attr('disabled', 'disabled');
+    $button.hide();
+  }
+
+  /* Returns the currently selected result. */
+  function getCurrentResult() {
+    var children = $results.children('li.' + options.selectedClass);
+    if (children.length)
+      return children;
+    return false;
+  }
+
+  /* Set the form to the currently selected result. */
+  function selectCurrentResult() {
+    var elt = getCurrentResult();
+    if (elt)
+      setResult(elt);
+  }
+
+  /* Set the form to the given result. */
+  function setResult(elt) {
+    $osm_id.val(elt.attr('id').substring(3));
+    $input.val(elt.html());
+    closeSuggest();
+    $button.removeAttr('disabled');
+    $button.show();
+  }
+
+  function setSelectedResultTo(elt) {
+    $results.children('li').removeClass(options.selectedClass);
+    if (elt)
+      elt.addClass(options.selectedClass);
+  }
+
+  function prevResult() {
+    var current = getCurrentResult();
+    var new_result;
+
+    if (current) {
+      var prev_valid = current.siblings('li.suggestok');
+      if (prev_valid.length)
+        new_result = prev_valid;
+    } else {
+      new_result = $results.children('li.suggestok:last');
+    }
+
+    setSelectedResultTo(new_result);
+  }
+
+  function nextResult() {
+    var current = getCurrentResult();
+    var new_result;
+
+    if (current) {
+      var next_valid = current.siblings('li.suggestok');
+      if (next_valid.length)
+        new_result = next_valid;
+    } else {
+      new_result = $results.children('li.suggestok:first');
+    }
+
+    setSelectedResultTo(new_result);
+  }
+
+  /* Empty and close the suggestion box. */
+  function closeSuggest() {
+    $results.empty();
+    $results.hide();
+  }
 }
 
