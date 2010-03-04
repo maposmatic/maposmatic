7cf186d290ede16b1ad03c3be9fb1054ab854c34
David Decotigny <d2@maposmatic.org>
2010-03-04 08:51:42 +0100

    Fixed unresolved symbol
    I receive annoying django error emails about EmptyPage being
    unresolved. This patch should fix this problem.
    
    Signed-off-by: Maxime Petazzoni <maxime.petazzoni@bulix.org>

--- www/maposmatic/views.py
+++ www/maposmatic/views.py
@@ -26,7 +26,7 @@
 
 import datetime
 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
