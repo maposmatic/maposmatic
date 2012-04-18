fa688a561167f8f11f6efbcd5f65f215dcd4251e
Thomas Petazzoni <thomas.petazzoni@enix.org>
2012-04-18 22:08:00 +0200

    Adapt template loaders to Django 1.4
    In Django 1.4, the function-style template loading is no longer
    possible, only the class-style template loading (available since
    Django 1.2) is possible.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@enix.org>

--- www/settings.py
+++ www/settings.py
@@ -74,8 +74,8 @@ SECRET_KEY = 'tm+wb)lp5q%br=p0d2toz&km_-w)cmcelv!7inons&^v9(q!d2'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
